0787f3be42bc9460af5ffd0249373f6acdb5bffd
Natasha K <kapr.natasha@gmail.com>
2018-07-19 21:08:00 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,11 @@
 class Artist
   attr_accessor :name
-  
   def initialize(name)
     @name = name
-    @song = song
-  end
-
-  def name
-  end
-
-  def songs
-
+    @song = []
   end
 
   def add_song(song)
-    @song << song
 
   end
 end
